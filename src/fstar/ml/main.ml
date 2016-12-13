29480ba111d73bbcaac427a163e92ee285b3ce85
Tahina Ramananandro <taramana@microsoft.com>
2016-12-13 11:49:05 -0800

    This branch REQUIRES opam switch 4.03.0+statistical-memprof
    This branch includes src/fstar/ml/memprofHelpers.ml, by
    Jacques-Henri Jourdan. Since proper licensing for this file is
    unknown, this branch and any of its descendants MUST NOT be
    publicly released and MUST NOT be merged into master or any F*
    release branch.
    
    For more information on Jourdan's statistical memory profiler:
    https://github.com/jhjourdan/ocaml/tree/memprof

--- src/fstar/ml/main.ml
+++ src/fstar/ml/main.ml
@@ -1,4 +1,14 @@
 let x = 
       Printexc.record_backtrace true;
       Gc.set { (Gc.get()) with Gc.minor_heap_size = 1048576; Gc.major_heap_increment = 4194304; Gc.space_overhead = 150; };
+      (* enable Jacques-Henri Jourdan's statistical memory profiler
+         (send SIGUSR1 to output results, which will create a
+         memory_profile file). For more info:
+         https://github.com/jhjourdan/ocaml/tree/memprof *)
+      let () =
+        let sampling_rate = 1E-4 in
+        let callstack_size = 20 in
+        let threshold = 100 in
+        MemprofHelpers.start sampling_rate callstack_size threshold
+      in
       FStar_FStar.main ()
