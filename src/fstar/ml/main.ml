6d24f3cca640dde8a6138cb743a0f0220d78c583
Tahina Ramananandro <taramana@microsoft.com>
2016-12-14 08:46:26 -0800

    Control memory profiling via environment variables

--- src/fstar/ml/main.ml
+++ src/fstar/ml/main.ml
@@ -7,9 +7,9 @@ let x =
          https://github.com/jhjourdan/ocaml/tree/memprof *)
       let () =
         let filename = Printf.sprintf "statistical-memprof-%d" (Unix.getpid ()) in
-        let sampling_rate = 1E-4 in
-        let callstack_size = 20 in
-        let threshold = 100 in
+        let sampling_rate = try float_of_string (Sys.getenv "MEMPROF_SAMPLING_RATE") with _ -> 1E-4 in
+        let callstack_size = try int_of_string (Sys.getenv "MEMPROF_CALLSTACK_SIZE") with _ -> 20 in
+        let threshold = try int_of_string (Sys.getenv "MEMPROF_THRESHOLD") with _ -> 100 in
         MemprofHelpers.start filename sampling_rate callstack_size threshold
       in
       FStar_FStar.main ()
